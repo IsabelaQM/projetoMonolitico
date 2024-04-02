171c8af6a04795e89e19e03f6645b824a60be4e4
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2024-04-02 10:20:57 -0300

    Update CategoriaRepository.java

--- manytoone/src/main/java/br/edu/iftm/manytoone/repository/CategoriaRepository.java
+++ manytoone/src/main/java/br/edu/iftm/manytoone/repository/CategoriaRepository.java
@@ -3,6 +3,6 @@ package br.edu.iftm.manytoone.repository;
 import org.springframework.data.repository.CrudRepository;
 import br.edu.iftm.manytoone.domain.Categoria;
 
-public interface CategoriaRepository extends CrudRepository<Categoria,Integer> {
+public interface CategoriaRepository extends CrudRepository<Categoria,Long> {
     
 }
