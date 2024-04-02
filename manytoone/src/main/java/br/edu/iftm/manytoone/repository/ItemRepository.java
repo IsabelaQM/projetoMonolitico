e92637a00a31ab9371d6c6c4eb30ebde571f2177
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2024-04-02 10:21:14 -0300

    Update ItemRepository.java

--- manytoone/src/main/java/br/edu/iftm/manytoone/repository/ItemRepository.java
+++ manytoone/src/main/java/br/edu/iftm/manytoone/repository/ItemRepository.java
@@ -3,6 +3,6 @@ package br.edu.iftm.manytoone.repository;
 import org.springframework.data.repository.CrudRepository;
 import br.edu.iftm.manytoone.domain.Item;
 
-public interface ItemRepository extends CrudRepository<Item,Integer> {
+public interface ItemRepository extends CrudRepository<Item,Long> {
     
 }
